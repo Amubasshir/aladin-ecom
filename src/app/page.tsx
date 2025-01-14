a597facfb130c2ef1c48c24a53df595287f2e022
Mubasshir Ahmed <lastmubasshir@gmail.com>
2025-01-14 14:49:01 +0600

    feat:add fonts, metadata and shadcn ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl text-red-500">Homepage</h1>
+      <h1 className="text-2xl text-red-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+        voluptatem magni id incidunt harum eius. Unde corrupti libero laborum
+        maiores et totam?
+      </h1>
+      <h1 className="text-2xl text-red-500 font-lato">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nemo?
+        Nemo saepe iure quam placeat quis deserunt hic tenetur amet ipsam totam?
+      </h1>
+      <Button variant="destructive" size="sm">
+        Click me
+      </Button>
     </div>
   );
 }
