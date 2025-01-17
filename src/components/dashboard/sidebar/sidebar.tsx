45861b41f3a967ba7103553be19740b0c5b7b677
Mubasshir Ahmed <lastmubasshir@gmail.com>
2025-01-17 11:59:16 +0600

    feat:user info added to sidebar

--- src/components/dashboard/sidebar/sidebar.tsx
+++ src/components/dashboard/sidebar/sidebar.tsx
@@ -1,16 +1,19 @@
 import Logo from '@/components/shared/logo';
+import { currentUser } from '@clerk/nextjs/server';
 import { FC } from 'react';
+import UserInfo from './user-into';
 
 interface SidebarProps {
   isAdmin?: boolean;
 }
 
 const CustomSidebar: FC<SidebarProps> = async ({}) => {
-  // const user = await currentUser();
+  const user = await currentUser();
   return (
     <div className="w-[300px] border-r h-screen p-4 flex flex-col fixed top-0 left-0 bottom-0  ">
       <Logo width="" height="" />
       <span className="ml-3" />
+      {user && <UserInfo user={user} />}
     </div>
   );
 };
