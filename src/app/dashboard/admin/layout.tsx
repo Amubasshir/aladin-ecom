b7179d9a0f9ec544eda3cf427c399f190b019817
Mubasshir Ahmed <lastmubasshir@gmail.com>
2025-01-17 10:38:22 +0600

    feat:admin dashboard header

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,7 +1,24 @@
+import Header from '@/components/dashboard/header/header';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-function AdminDashboardLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+async function AdminDashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await currentUser();
+  if (!user || user.privateMetadata.role !== 'ADMIN') redirect('/');
+  return (
+    <div className="w-full h-full">
+      {/* {sidebar} */}
+      <div className="w-full ml-[300px]">
+        <Header />
+        <div className="w-full mt-[75px] p-4">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminDashboardLayout;
